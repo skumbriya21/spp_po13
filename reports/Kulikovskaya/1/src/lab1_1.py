cbb192d767a825b6845b3fd1d40f51fdf6b1f8b7
Alina <154704411+skumbriya21@users.noreply.github.com>
2026-02-06 08:42:50 +0100

    the style has been corrected in lab1_1.py

--- reports/Kulikovskaya/1/src/lab1_1.py
+++ reports/Kulikovskaya/1/src/lab1_1.py
@@ -2,10 +2,10 @@
 # вывод процента чисел, которые больше среднего значения
 
 N = list(map(int, input("Введите последовательность чисел: ").split()))
-averageN = sum(N) / len (N) if N else 0
+averageN = sum(N) / len(N) if N else 0
 count_above_average = 0
 for num in N:
     if num > averageN:
         count_above_average += 1
 percentage = round(((count_above_average / len(N)) * 100), 2)
-print (f"percent= {percentage}%")
+print(f"percent= {percentage}%")
